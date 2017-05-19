d34469382ef9f4f04a2ec16eb4d97488d45706d0
Enzo Hernan NIcolorich <enzonicolorich@gmail.com>
2017-05-18 21:36:19 -0300

    Finished user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,10 +6,80 @@
  */
 
 module.exports = {
-	index: function(req, res) {
-        return res.json({
-            test: 'testing index!'
+	create: function(req, res, next) {
+        var params = req.params.all();
+        User.create(params, function(err, user) {
+            if (err) return next(err);
+
+            res.status(201);
+
+            res.json(user);
         });
+    },
+    find: function(req, res, next) {
+        var where = req.param('where');
+        if (_.isString(where)) {
+            where = JSON.parse(where);
+        }
+
+        var options = {
+            limit: req.param('limit') || undefined,
+            skip: req.param('skip') || undefined,
+            sort: req.param('sort') || undefined,
+            where: where || undefined
+        };
+
+        User.find(options, function(err, users) {
+            if (err) return next(err);
+
+            res.json(users);
+        })
+    },
+    findOne: function(req, res, next) {
+        var id = req.param('id');
+        User.findOne(id, function(err, user) {
+            if (user === undefined) return res.notFound();
+
+            if (err) return next(err);
+
+            res.json(user);
+        })
+    },
+    update: function(req, res, next) {
+        var id = req.param('id');
+
+        if (!id) {
+            return res.badRequest('No id provided.');
+        }
+
+        var criteria = {};
+        criteria = _.merge({}, req.params.all(), req.body);
+
+        User.update(id, criteria, function(err, user) {
+            if (user.length === 0) return res.notFound;
+            if (err) return next(err);
+
+            res.json(user);
+        })
+    },
+    destroy: function(req, res, next) {
+        var id = req.param('id');
+
+        if (!id) {
+            return res.badRequest('No id provided.');
+        }
+
+        User.findOne(id, function(err, user) {
+            if (err) return res.serverError(err);
+
+            if (!user) return res.notFound();
+
+            User.destroy(id, function(err) {
+                if (err) return next(err);
+
+                return res.json(user);
+            })
+        })
     }
 };
 
